9fa7d2190c87dc73ddb6812ab0a981f9dd26fead
YS <yongsheng@challenger.sg>
2023-11-21 19:54:36 +0800

    console email

--- index.mjs
+++ index.mjs
@@ -4,6 +4,10 @@ const ts = new Date().toLocaleString('en-US', {timeZone: 'Asia/Singapore'});
 export const handler = async (event) => {
 
     console.log(event)
+    
+    let body = JSON.parse(event.body);
+    console.log('body',body)
+    console.log('body.email',body.email)
 
     const response = {
         statusCode: 200,
